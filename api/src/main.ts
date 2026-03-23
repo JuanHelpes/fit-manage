84a118013f296a9b594bd38fd7a66841541768af
Juan Helpes <helpeshs@gmail.com>
2026-03-23 10:01:22 -0300

    feat: enable CORS in main.ts and update baseline-browser-mapping version in package.json

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,6 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  //cors 
+  app.enableCors({
+    origin: 'http://localhost:5173', // sua origem do Vite/React
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true, // se usar cookies/auth headers
+  });
+
   // Pipes
   app.useGlobalPipes(
     new ValidationPipe({
